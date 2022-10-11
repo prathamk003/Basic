8f5dfc3c0d674ad400bd07a473b1e48140b2388b
prathamk003 <98006076+prathamk003@users.noreply.github.com>
2022-10-11 20:31:12 +0530

    Create quotientremain.cpp

---
+++ quotientremain.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int n,m;
+    cout << "Enter dividend and divisor" << endl;
+    cin >> n >> m;
+    cout << "Quotient = " << n/m << endl << "Remainder = " << n%m << endl;
+}
